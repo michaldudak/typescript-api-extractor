ca0e8b029f2ee6f9ea97173fa8d2e088daa84e91
dav-is <mail@connordav.is>
2025-12-30 22:35:51 -0500

    Add more test cases

--- test/type-aliases/input.ts
+++ test/type-aliases/input.ts
@@ -21,3 +21,12 @@ interface BaseInterface {
 export type ExtendedAlias = BaseInterface & {
 	extra: boolean;
 };
+
+// Test that type aliases are preserved when used in function parameters
+export function processWithUnionAlias(mode: UnionAlias): string {
+	return mode;
+}
+
+export function processWithObjectAlias(obj: ObjectAlias): string {
+	return obj.name;
+}
