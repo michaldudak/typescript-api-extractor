ecd58b68c4df0b96f03eabf277567eb0a7cfd5f7
dav-is <mail@connordav.is>
2026-01-02 10:00:18 -0500

    Restore mui-overridable-component test

--- test/dts-declarations/input.d.ts
+++ test/dts-declarations/input.d.ts
@@ -1,7 +1,6 @@
 // This file tests .d.ts declaration files with various patterns
-// Combines: reexports-from-js (importing from .js in d.ts) and mui-overridable-component patterns
+// Tests re-exports from .js files (simulates built output)
 
-// Part 1: Re-exports from .js files (simulates built output)
 import {
 	FormattedProperty,
 	FormattedParameter,
@@ -28,55 +27,3 @@ export function processType(input: FormattedParameter): FormattedProperty;
 
 // Function using re-exported generic type
 export function wrapValue<T>(value: T): Container<T>;
-
-// Part 2: Material UI's OverridableComponent pattern
-declare const Component: OverridableComponent<ComponentTypeMap> & { muiName: string };
-
-export { Component };
-
-interface ComponentOwnProps {
-	variant?: 'default' | 'small' | 'large';
-}
-
-interface ComponentTypeMap<AdditionalProps = {}, RootComponent extends React.ElementType = 'span'> {
-	props: AdditionalProps & ComponentOwnProps;
-	defaultComponent: RootComponent;
-}
-
-interface OverridableComponent<TypeMap extends OverridableTypeMap> {
-	<RootComponent extends React.ElementType>(
-		props: {
-			/**
-			 * The component used for the root node.
-			 */
-			component: RootComponent;
-		} & OverrideProps<TypeMap, RootComponent>,
-	): React.JSX.Element | null;
-	(props: DefaultComponentProps<TypeMap>): React.JSX.Element | null;
-}
-
-type OverrideProps<
-	TypeMap extends OverridableTypeMap,
-	RootComponent extends React.ElementType,
-> = BaseProps<TypeMap> &
-	DistributiveOmit<React.ComponentPropsWithRef<RootComponent>, keyof BaseProps<TypeMap>>;
-
-type DefaultComponentProps<TypeMap extends OverridableTypeMap> = BaseProps<TypeMap> &
-	DistributiveOmit<
-		React.ComponentPropsWithRef<TypeMap['defaultComponent']>,
-		keyof BaseProps<TypeMap>
-	>;
-
-interface OverridableTypeMap {
-	props: {};
-	defaultComponent: React.ElementType;
-}
-
-type BaseProps<TypeMap extends OverridableTypeMap> = TypeMap['props'] & CommonProps;
-
-interface CommonProps {
-	className?: string;
-	style?: React.CSSProperties;
-}
-
-type DistributiveOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never;
---
+++ test/mui-overridable-component/input.d.ts
@@ -0,0 +1,56 @@
+// Material UI's OverridableComponent
+
+declare const Component: OverridableComponent<ComponentTypeMap> & { muiName: string };
+
+export default Component;
+
+interface ComponentOwnProps {
+	variant?: 'default' | 'small' | 'large';
+}
+
+interface ComponentTypeMap<AdditionalProps = {}, RootComponent extends React.ElementType = 'span'> {
+	props: AdditionalProps & ComponentOwnProps;
+	defaultComponent: RootComponent;
+}
+
+interface OverridableComponent<TypeMap extends OverridableTypeMap> {
+	// If you make any changes to this interface, please make sure to update the
+	// `OverridableComponent` type in `mui-types/index.d.ts` as well.
+	// Also, there are types in Base UI that have a similar shape to this interface
+	// (for example SelectType, OptionType, etc.).
+	<RootComponent extends React.ElementType>(
+		props: {
+			/**
+			 * The component used for the root node.
+			 */
+			component: RootComponent;
+		} & OverrideProps<TypeMap, RootComponent>,
+	): React.JSX.Element | null;
+	(props: DefaultComponentProps<TypeMap>): React.JSX.Element | null;
+}
+
+type OverrideProps<
+	TypeMap extends OverridableTypeMap,
+	RootComponent extends React.ElementType,
+> = BaseProps<TypeMap> &
+	DistributiveOmit<React.ComponentPropsWithRef<RootComponent>, keyof BaseProps<TypeMap>>;
+
+type DefaultComponentProps<TypeMap extends OverridableTypeMap> = BaseProps<TypeMap> &
+	DistributiveOmit<
+		React.ComponentPropsWithRef<TypeMap['defaultComponent']>,
+		keyof BaseProps<TypeMap>
+	>;
+
+interface OverridableTypeMap {
+	props: {};
+	defaultComponent: React.ElementType;
+}
+
+type BaseProps<TypeMap extends OverridableTypeMap> = TypeMap['props'] & CommonProps;
+
+interface CommonProps {
+	className?: string;
+	style?: React.CSSProperties;
+}
+
+type DistributiveOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never;
