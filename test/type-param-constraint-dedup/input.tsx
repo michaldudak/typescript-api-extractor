e40dff82b92d7862b2239338563ead8c6ec1b7a0
dav-is <mail@connordav.is>
2026-01-19 13:40:48 -0500

    Prettier

--- test/type-param-constraint-dedup/input.tsx
+++ test/type-param-constraint-dedup/input.tsx
@@ -4,29 +4,29 @@ import * as React from 'react';
 // WITHOUT a constraint in the BaseUIComponentProps definition
 
 namespace Accordion {
-  export namespace Panel {
-    export interface State {
-      open: boolean;
-      disabled: boolean;
-    }
-  }
+	export namespace Panel {
+		export interface State {
+			open: boolean;
+			disabled: boolean;
+		}
+	}
 }
 
 // This is how Base UI defines component props - State is just a type parameter
 // with NO constraint at the definition site
 type BaseUIComponentProps<
-  ElementType extends React.ElementType,
-  State,
-  RenderFunctionProps = React.HTMLAttributes<HTMLElement>,
+	ElementType extends React.ElementType,
+	State,
+	RenderFunctionProps = React.HTMLAttributes<HTMLElement>,
 > = {
-  /**
-   * className callback - State has no constraint here
-   */
-  className?: string | ((state: State) => string | undefined);
-  /**
-   * render callback - State has no constraint here
-   */
-  render?: React.ReactElement | ((props: RenderFunctionProps, state: State) => React.ReactElement);
+	/**
+	 * className callback - State has no constraint here
+	 */
+	className?: string | ((state: State) => string | undefined);
+	/**
+	 * render callback - State has no constraint here
+	 */
+	render?: React.ReactElement | ((props: RenderFunctionProps, state: State) => React.ReactElement);
 };
 
 // When actually used, State is instantiated with a concrete type
