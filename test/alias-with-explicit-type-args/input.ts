81240e9fde607cbccaad4f94e7e96520181d9d87
dav-is <mail@connordav.is>
2026-03-11 21:05:07 -0400

    Fix coverage gap

--- test/alias-with-explicit-type-args/input.ts
+++ test/alias-with-explicit-type-args/input.ts
@@ -27,4 +27,8 @@ export interface Component {
 	directDefault: Outer<boolean, bigint>;
 	// Direct usage with non-default args
 	directOverride: Outer<string, number>;
+	// Partially explicit: first arg overridden, second uses Outer default (bigint)
+	partialOverride: Outer<string>;
+	// Partially explicit: first arg matches Outer default, second uses Outer default
+	partialDefault: Outer<boolean>;
 }
